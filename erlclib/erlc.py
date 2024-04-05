d0a945d229b081c2cc31d5e9093e8c3ae193e2b3
Fin-GITHUB <70870542+fin-github@users.noreply.github.com>
2024-04-05 18:03:07 -0400

    Update erlc.py
    update

--- erlclib/erlc.py
+++ erlclib/erlc.py
@@ -14,20 +14,28 @@ from time import sleep as wait
 
 class ERLC:
     def __init__(self,
-                 key: str):
+                 key: str,
+                 ratelimit_key: str = None):
         self.key = key
+        self.ratelimit_key = ratelimit_key
     
     def get_status(self):
         response = send_request(
             "GET",
             part="",
-            key=self.key
+            key=self.key,
+            ratelimit_key=self.ratelimit_key
         )
         # Set the json variable to the json of the request
         json = response.json()
         
         # Create the name variable
         name = json["Name"]
+        
+        # owner
+        owner = Player(
+            id=json["OwnerId"]
+        )
         
         # Create the name vars
         co_owner_ids = json["CoOwnerIds"]
@@ -56,6 +64,7 @@ class ERLC:
         
         return ServerStatus(
             name=json["Name"],
+            owner=owner,
             co_owners=co_owners,
             co_owner_ids=co_owner_id,
             amount_of_players=amount_of_players,
@@ -65,19 +74,14 @@ class ERLC:
             team_balance_enabled=team_balance_enabled
         )
         
-    def get_amount_of_players(self):
-        return len(send_request(
-            "GET",
-            part="players",
-            key=self.key
-        ))
-        
     def get_players(self):
         raw_players = send_request(
             "GET",
             part="players",
-            key=self.key
+            key=self.key,
+            ratelimit_key=self.ratelimit_key
         ).json()
+        if not type(raw_players) is dict: print(f"Incorrect type! {type(raw_players)}")
         
         
         players: list[playerclass.Player] = []
@@ -103,12 +107,16 @@ class ERLC:
                 )
             )
         return players
+    
+    def get_amount_of_players(self):
+        return len(self.get_players())
             
     def get_join_logs(self):
         raw_join_logs = send_request(
             "GET",
             part="joinlogs",
-            key=self.key
+            key=self.key,
+            ratelimit_key=self.ratelimit_key
         ).json()
         
         join_logs: list[JoinLog] = []
@@ -116,7 +124,7 @@ class ERLC:
         for join_log_dict in raw_join_logs:
             status: Join | Leave = Join if join_log_dict["Join"] else Leave
             date: datetime = datetime.fromtimestamp(join_log_dict["Timestamp"])
-            playername, playerid = join_log_dict["Player"].split(";")
+            playername, playerid = join_log_dict["Player"].split(":")
             player: playerclass.Player = playerclass.Player(
                 name=playername,
                 id=playerid
@@ -134,7 +142,8 @@ class ERLC:
         raw_cmd_logs = send_request(
             "GET",
             part="commandlogs",
-            key=self.key
+            key=self.key,
+            ratelimit_key=self.ratelimit_key
         )
         ic(raw_cmd_logs)
         raw_cmd_logs = raw_cmd_logs.json()
@@ -142,7 +151,10 @@ class ERLC:
         cmd_logs: list[CommandLog] = []
         
         for cmd_log_dict in raw_cmd_logs:
-            date: datetime = datetime.fromtimestamp(cmd_log_dict["Timestamp"])
+            try:
+                date: datetime = datetime.fromtimestamp(cmd_log_dict["Timestamp"])
+            except TypeError:
+                date: datetime = datetime.fromtimestamp(int(cmd_log_dict["Timestamp"]))
             try:
                 playername, playerid = cmd_log_dict["Player"].split(":")
             except ValueError:
@@ -178,7 +190,8 @@ class ERLC:
                     command: str): 
         response = send_command_request(
             command=command,
-            key=self.key
+            key=self.key,
+            ratelimit_key=self.ratelimit_key
         )
         
         match response.status_code:
